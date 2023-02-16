2a32f672761744b91861d90f45d2ac9906af3399
aires1294 <pedrotja.engenharia@gmail.com>
2023-02-16 19:06:07 -0300

    item 2 testes acho que ta pronto

---
+++ tests/unit/controllers/products.controllers.test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const sinon = require('sinon');
+
+const productsService = require('../../../src/services/products.service');
+const productsController = require('../../../src/controllers/products.controller');
+const products = require('./mock.controller');
+const { func } = require('joi');
+
+chai.use(sinonChai);
+const { expect } = chai;
+
+describe('Controller de products', function () {
+  describe('Lista todos os produtos', function () {
+    it('Deve retornar uma resposta com status 200 e todos os produtos', async function () {
+      const req = {};
+      const res = {};
+      res.status = sinon.stub.returns(res);
+      res.json = sinon.stub.returns();
+      sinon.stub(productsService, 'getAllProducts').resolves({ type: 200, message: products });
+      await productsController.getAllProducts(req, res);
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWithExactly(products);
+    })
+    it('Deve retornar uma resposta com status 200 e o produto pelo ID', async function () {
+      const req = { params: { id: 1 } };
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsService, 'getProductById').resolves(products[0]);
+      await productsController.getProductById(req, res);
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWithExactly(products[0]);
+      
+    })
+  })
+})
