793fd325c6dd7c9227a2fe8e150a9331b30e7cc4
aires1294 <pedrotja.engenharia@gmail.com>
2023-03-08 19:16:13 -0300

    atualizando connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE || 'StoreManager',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE || 'StoreManager',
+  port: process.env.MYSQLPORT,
 }); 
 
 module.exports = { connection };
